7729cc522fd88c6a92d1897434c0eaa6b177e7ae
Edward Krueger <edkrueger@gmail.com>
2019-08-07 15:32:13 -0500

    Add a failing subtraction test.

--- math/math.test.js
+++ math/math.test.js
@@ -3,3 +3,7 @@ const math = require ('./math')
 test('adds 1 + 2 to equal 3', () => {
 	expect(math.add(1,2)).toBe(3)
 })
+
+test('subtracts 1 - 2 to equal -1', () => {
+	expect(math.subtract(1, 2)).toBe(-1)
+})
